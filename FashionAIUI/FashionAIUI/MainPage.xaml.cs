2317874d4e4776f10b0345a027647f61740a0d42
Jasmine C <happylandrox@gmail.com>
2018-07-20 10:10:26 -0700

    Added picture labels

--- FashionAIUI/FashionAIUI/MainPage.xaml.cs
+++ FashionAIUI/FashionAIUI/MainPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,109 +27,156 @@ namespace FashionAIUI
     /// </summary>
     public sealed partial class MainPage : Page
     {
-       
 
-            Boolean pic1;
-            Boolean pic2;
-            public MainPage()
+
+        Boolean pic1;
+        Boolean pic2;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Button1.Visibility = Visibility.Visible;
+            Button2.Visibility = Visibility.Visible;
+            MixBtn.Visibility = Visibility.Collapsed;
+            pic1 = false;
+            pic2 = false;
+        }
+
+
+
+        private async void UploadPic(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
             {
-                this.InitializeComponent();
-                Button1.Visibility = Visibility.Visible;
-                Button2.Visibility = Visibility.Visible;
-                MixBtn.Visibility = Visibility.Collapsed;
-                pic1 = false;
-                pic2 = false;
-            }
-
-
-
-            private async void UploadPic(object sender, RoutedEventArgs e)
-            {
-                var picker = new Windows.Storage.Pickers.FileOpenPicker();
-                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-                picker.FileTypeFilter.Add(".jpg");
-                picker.FileTypeFilter.Add(".jpeg");
-                picker.FileTypeFilter.Add(".png");
-
-                Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
-                if (file != null)
+                //imagePath.Text = file.Path;
+                using (Windows.Storage.Streams.IRandomAccessStream fileStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
                 {
+                    // Set the image source to the selected bitmap. 
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(fileStream);
+                    uploadedImage.Source = bitmapImage;
+                    Button1.Visibility = Visibility.Collapsed;
+                    pic1 = true;
 
-                    // Application now has read/write access to the picked file 
-                    //imagePath.Text = file.Path;
-
-                    // Open a stream for the selected file. 
-                    // The 'using' block ensures the stream is disposed 
-                    // after the image is loaded. 
-                    using (Windows.Storage.Streams.IRandomAccessStream fileStream =
-                        await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                    if (pic1 && pic2)
                     {
-                        // Set the image source to the selected bitmap. 
-                        BitmapImage bitmapImage = new BitmapImage();
-                        bitmapImage.SetSource(fileStream);
-                        uploadedImage.Source = bitmapImage;
-                        Button1.Visibility = Visibility.Collapsed;
-                        pic1 = true;
-
-                        if (pic1 && pic2)
-                        {
-                            MixBtn.Visibility = Visibility.Visible;
-                        }
-
+                        MixBtn.Visibility = Visibility.Visible;
                     }
+
                 }
-
-            }
-
-            private async void UploadPic2(object sender, RoutedEventArgs e)
-            {
-                var picker = new Windows.Storage.Pickers.FileOpenPicker();
-                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-                picker.FileTypeFilter.Add(".jpg");
-                picker.FileTypeFilter.Add(".jpeg");
-                picker.FileTypeFilter.Add(".png");
-
-                Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
-                if (file != null)
-                {
-
-                    // Application now has read/write access to the picked file 
-                    //imagePath.Text = file.Path;
-
-                    // Open a stream for the selected file. 
-                    // The 'using' block ensures the stream is disposed 
-                    // after the image is loaded. 
-                    using (Windows.Storage.Streams.IRandomAccessStream fileStream =
-                        await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
-                    {
-                        BitmapImage bitmapImage = new BitmapImage();
-                        bitmapImage.SetSource(fileStream);
-                        uploadedImage2.Source = bitmapImage;
-                        Button2.Visibility = Visibility.Collapsed;
-                        pic2 = true;
-
-                        if (pic1 && pic2)
-                        {
-                            MixBtn.Visibility = Visibility.Visible;
-                        }
-                    }
-                }
-
-            }
-
-            public void Reset(object sender, RoutedEventArgs e)
-            {
-                Button1.Visibility = Visibility.Visible;
-                Button2.Visibility = Visibility.Visible;
-                uploadedImage.Source = null;
-                uploadedImage2.Source = null;
-                pic1 = false;
-                pic2 = false;
-                MixBtn.Visibility = Visibility.Collapsed;
             }
 
         }
-    
+
+        private async void UploadPic2(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                //imagePath.Text = file.Path;
+                using (Windows.Storage.Streams.IRandomAccessStream fileStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(fileStream);
+                    uploadedImage2.Source = bitmapImage;
+                    Button2.Visibility = Visibility.Collapsed;
+                    pic2 = true;
+
+                    if (pic1 && pic2)
+                    {
+                        MixBtn.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+
+        }
+
+        public void Reset(object sender, RoutedEventArgs e)
+        {
+            Button1.Visibility = Visibility.Visible;
+            Button2.Visibility = Visibility.Visible;
+            uploadedImage.Source = null;
+            uploadedImage2.Source = null;
+            pic1 = false;
+            pic2 = false;
+            MixBtn.Visibility = Visibility.Collapsed;
+        }
+
+        public async void FilePicker(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker fileSavePicker = new FileSavePicker();
+            fileSavePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileSavePicker.FileTypeChoices.Add("JPEG files", new List<string>() { ".jpg" });
+            fileSavePicker.SuggestedFileName = "image";
+
+            var outputFile = await fileSavePicker.PickSaveFileAsync();
+
+            if (outputFile == null)
+            {
+                // The user cancelled the picking operation
+                return;
+            }
+        }
+
+        private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+        {
+            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                // Create an encoder with the desired format
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+
+                // Set the software bitmap
+                encoder.SetSoftwareBitmap(softwareBitmap);
+
+                // Set additional encoding parameters, if needed
+                encoder.BitmapTransform.ScaledWidth = 320;
+                encoder.BitmapTransform.ScaledHeight = 240;
+                encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
+                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Fant;
+                encoder.IsThumbnailGenerated = true;
+
+                try
+                {
+                    await encoder.FlushAsync();
+                }
+                catch (Exception err)
+                {
+                    const int WINCODEC_ERR_UNSUPPORTEDOPERATION = unchecked((int)0x88982F81);
+                    switch (err.HResult)
+                    {
+                        case WINCODEC_ERR_UNSUPPORTEDOPERATION:
+                            // If the encoder does not support writing a thumbnail, then try again
+                            // but disable thumbnail generation.
+                            encoder.IsThumbnailGenerated = false;
+                            break;
+                        default:
+                            throw;
+                    }
+                }
+
+                if (encoder.IsThumbnailGenerated == false)
+                {
+                    await encoder.FlushAsync();
+                }
+
+
+            }
+        }
+    }
 }
